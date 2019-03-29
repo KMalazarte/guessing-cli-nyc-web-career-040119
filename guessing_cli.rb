29c73bec61d0aa57b50bad66b9bc1e11130ac817
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-29 22:26:16 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,7 +10,7 @@ def run_guessing_game
     if input == "exit"
       puts "Goodbye!"
     elsif input.to_i == correct_num
-      puts "You guessed the correct number"
+      puts "You guessed the correct number!"
     else 
       puts "The computer guessed #{correct_num}."
     end
